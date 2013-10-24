d84a1d0bbf78fb5456319bd33b5a8c0683c5d521
Greg Green <ggreen@bit-builder.com>
2013-10-23 18:31:08 -0700

    Initial repository load

---
+++ gpg-python-template.el
@@ -0,0 +1,28 @@
+;;; gpg-python-template.el
+;;;
+
+(require 'tempo)
+
+;;;
+;;; Define some templates
+
+;; A module template
+
+(tempo-define-template "gpg-python-skeleton" 
+  '("##############################################################################\n"
+    "# \n"
+    "# Copyright (c) 2005 Greg Green <ggreen@bit-builder.com>\n"
+    "# \n"
+    "# File: \n"
+    "'''\n" (P "Module doc string: ") "\n'''\n"
+    "\n\n")
+  "moduleSkeleton"
+  "Insert a skeleton for a Python module.")
+
+;;
+;; Functions to insert the templates
+
+(defun gpg-python-insert-new-buffer-strings ()
+  "Insert gpg-python-insert-new-buffer-strings."
+  (interactive)
+  (tempo-template-gpg-python-skeleton))
