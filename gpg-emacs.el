46f559ba49c71a4d612067ae523266daf2bd19f1
Greg Green <gpgreen@gmail.com>
2018-10-22 20:58:16 -0700

    Working on semantic

--- gpg-emacs.el
+++ gpg-emacs.el
@@ -29,33 +29,77 @@
 ;; See cedet/common/cedet.info for configuration details.
 ;; IMPORTANT: Tou must place this *before* any CEDET component
 ;; gets activated by another package (Gnus, auth-source, ...).
-(load-file "/home/ggreen/src/cedet/cedet-devel-load.el")
-(load-file "/home/ggreen/src/cedet/contrib/cedet-contrib-load.el")
+(setq cedet-root-path (file-name-as-directory "~/src/cedet/"))
+(load-file (concat cedet-root-path "cedet-devel-load.el"))
+(load-file (concat cedet-root-path "contrib/cedet-contrib-load.el"))
 
-;; Add further minor-modes to be enabled by semantic-mode.
-;; See doc-string of `semantic-default-submodes' for other things
-;; you can use here.
-(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
-(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
-(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
+;; select which submodes we want to activate
+(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode)
+;(add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
+(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode)
 
 ;; Enable Semantic
 (semantic-mode 1)
 
-;; Enable EDE (Project Management) features
-(global-ede-mode 1)
+;; load contrib library
+(require 'eassist)
+
+;; advanced functionality for name completion
+(require 'semantic/ia)
+
+;; system header files via gcc
+(require 'semantic/bovine/gcc)
 
 ;; turn on column numbers
 (setq column-number-mode t)
 
+;; customization of modes
+(defun alexott/cedet-hook ()
+  (local-set-key [(control return)] 'semantic-ia-complete-symbol-menu)
+  (local-set-key "\C-c?" 'semantic-ia-complete-symbol)
+  ;;
+  (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
+  (local-set-key "\C-c=" 'semantic-decoration-include-visit)
+
+  (local-set-key "\C-cj" 'semantic-ia-fast-jump)
+  (local-set-key "\C-cq" 'semantic-ia-show-doc)
+  (local-set-key "\C-cs" 'semantic-ia-show-summary)
+  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+  )
+(add-hook 'c-mode-common-hook 'alexott/cedet-hook)
+(add-hook 'lisp-mode-hook 'alexott/cedet-hook)
+(add-hook 'scheme-mode-hook 'alexott/cedet-hook)
+(add-hook 'emacs-lisp-mode-hook 'alexott/cedet-hook)
+(add-hook 'erlang-mode-hook 'alexott/cedet-hook)
+
+(defun alexott/c-mode-cedet-hook ()
+  (local-set-key "\C-ct" 'eassist-switch-h-cpp)
+  (local-set-key "\C-xt" 'eassist-switch-h-cpp)
+  (local-set-key "\C-ce" 'eassist-list-methods)
+  (local-set-key "\C-c\C-r" 'semantic-symref)
+  )
+(add-hook 'c-mode-common-hook 'alexott/c-mode-cedet-hook)
+
+(semanticdb-enable-gnu-global-databases 'c-mode t)
+(semanticdb-enable-gnu-global-databases 'c++-mode t)
+
+(when (cedet-ectag-version-check t)
+  (semantic-load-enable-primary-ectags-support))
+
+;; SRecode
+(global-srecode-minor-mode 1)
+
+;; Enable EDE (Project Management) features
+(global-ede-mode 1)
+;(ede-enable-generic-projects)
+
 ;; set the path
-(setq load-path 
-      (nconc
-       load-path
-       (list
-	"~/emacs"
-	"~/go/misc/emacs"
-	"~/lib/erlang/lib/tools-2.6.6.4/emacs")))
+(add-to-list 'load-path "~/emacs")
 
 ;; Magit
 (global-set-key (kbd "C-x g") 'magit-status)
@@ -355,6 +399,20 @@
 
 ;;; binary diff
 (load "binary-diff")
+
+;; EDE
+(ede-cpp-root-project "AHRS"
+                :name "Attitude Heading Reference Firmware"
+                :file "~/src/avfirmware/ahrs/ahrs.c"
+                :include-path '("/"
+                                "/../libs/avr_drivers/drivers"
+                                "/../libs/avr_drivers/utils"
+                                "/../libs/avr_drivers/i2cmaster"
+                                "/../libs/libcanard"
+                               )
+                :system-include-path '("/usr/include")
+                :spp-table '(("isUnix" . "")
+                             ("BOOST_TEST_DYN_LINK" . "")))
 
 ;;;;;;;;;;
 ;; the end
