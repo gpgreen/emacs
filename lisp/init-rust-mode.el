05a64b19b8efab192b2d4903c30fdfe669df55be
Greg Green <gpgreen@gmail.com>
2022-12-06 13:41:29 -0800

    removed lsp-ui from rust mode

--- lisp/init-rust-mode.el
+++ lisp/init-rust-mode.el
@@ -16,9 +16,17 @@
   (lsp-rust-analyzer-cargo-watch-command "clippy")
   (lsp-eldoc-render-all t)
   (lsp-idle-delay 0.6)
+  ;; enable/disable hints
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
   :config
-  (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.embuild\\'")
-  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+  (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.embuild\\'"))
+;  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
 (use-package lsp-ui
   :commands lsp-ui-mode
